23d56d0736a327ac6c7085cb62a0d3ee2b6051da
tiaki-py <noxios27@gmail.com>
2025-12-12 15:52:48 +0100

    button checks

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
         button = QPushButton("Press Me!")
-
+        button.setCheckable(True)
+        button.clicked.connect(self.on_button_click)
+        button.clicked.connect(self.on_button_toggle)
 
         self.setMinimumSize(QSize(100,100))
         self.setMaximumSize(QSize(3840,1980))
 
         self.setCentralWidget(button)
+    
+    def on_button_click(self):
+        print("Quelle jour es tu?")
+    
+    def on_button_toggle(self, checked):
+        print("Checked?", checked)
 
 
 app = QApplication(sys.argv)
