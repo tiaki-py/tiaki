a53bd67944d69fbb48c98f4a96ee18b52e5ea862
tiaki-py <noxios27@gmail.com>
2026-01-07 10:34:47 +0100

    context menus
    QCONTEXTACTION QCONTEXTACTION QCONTEXTACTION

--- app.py
+++ app.py
@@ -1,51 +1,19 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+from PyQt5.QtWidgets import QAction, QApplication, QLabel, QMainWindow, QMenu
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setMouseTracking(True)
-        self.label = QLabel("Click this window")
-        self.label.setMouseTracking(True)
-        self.setCentralWidget(self.label)
-    
-    def mouseMoveEvent(self, e):
-        self.label.setText("mouseMoveEvent")
 
-    def mousePressEvent(self, e):
-        if e.button() == Qt.LeftButton:
-            self.label.setText("LEFTmousepressevent")
-        
-        elif e.button() == Qt.MiddleButton:
-            self.label.setText("MIDDLEmousepressevent")
-
-        elif e.button() == Qt.RightButton:
-            self.label.setText("RIGHTmousepressevent")
-
-
-    def mouseReleaseEvent(self, e):
-        if e.button() == Qt.LeftButton:
-            self.label.setText("LEFTmousereleaseevent")
-        
-        elif e.button() == Qt.MiddleButton:
-            self.label.setText("MIDDLEmousereleaseevent")
-
-        elif e.button() == Qt.RightButton:
-            self.label.setText("RIGHTmousereleaseevent")
-
-
-    def mouseDoubleClickEvent(self, e):
-        if e.button() == Qt.LeftButton:
-            self.label.setText("LEFTmouseDoubleclickeEvent")
-        
-        elif e.button() == Qt.MiddleButton:
-            self.label.setText("MIDDLEmouseDoubleclickeEvent")
-
-        elif e.button() == Qt.RightButton:
-            self.label.setText("RIGHTmouseDoubleclickEvent")
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(QAction("QContextAction1", self))
+        context.addAction(QAction("QContextAction2", self))
+        context.addAction(QAction("QContextAction3", self))
+        context.exec(e.globalPos())
 
 
 app = QApplication(sys.argv)
